63c4083518e2f475f1edbb8574924061c743be93
Bence <Soti2200@gmail.com>
2024-05-25 09:12:49 +0200

    osszegzes tetele

--- src/gitesfeladat/GitesFeladat.java
+++ src/gitesfeladat/GitesFeladat.java
@@ -11,8 +11,10 @@ public class GitesFeladat {
 
     private static void osszegzesTetele() {
         int[] tomb = {3, 5, 6, 7};
-        
-        
+        int osszeg = 0;
+        for (int i = 0; i < tomb.length; i++) {
+            osszeg += tomb[i];
+        }
     }
     
 }
