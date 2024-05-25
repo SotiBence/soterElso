aa24070a99ff8c2ccc4d49c738bd97f3a002fad4
Bence <Soti2200@gmail.com>
2024-05-25 09:10:51 +0200

    tomb letrehozasa

---
+++ src/gitesfeladat/GitesFeladat.java
@@ -0,0 +1,18 @@
+
+package gitesfeladat;
+
+
+public class GitesFeladat {
+
+
+    public static void main(String[] args) {
+        osszegzesTetele();
+    }
+
+    private static void osszegzesTetele() {
+        int[] tomb = {3, 5, 6, 7};
+        
+        
+    }
+    
+}
